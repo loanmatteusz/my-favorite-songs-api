85957ed449b4c39ca3b30e314c8b3ca783248016
dev20 <dev.20@pbhub.com.br>
2023-01-15 19:24:08 -0300

    add: repository to providers

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/infra/prisma.service';
+import { UserRepository } from 'src/repositories/user-reposiroty';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService]
+  providers: [UserService, PrismaService, UserRepository]
 })
 export class UserModule {}
