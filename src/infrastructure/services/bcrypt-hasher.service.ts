69cf55ba6982b0831c0dd14885bd618df249a8c1
loanmatteusz <loan.matteusz@gmail.com>
2025-08-28 22:23:09 -0300

    refactor: rename function bcrypt password hasher to bcrypt hasher

--- src/infrastructure/services/bcrypt-hasher.service.ts
+++ src/infrastructure/services/bcrypt-hasher.service.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import { PasswordHasher } from "../../domain/services/password-hasher.service";
 
-export const bcryptPasswordHasher: PasswordHasher = {
+export const bcryptHasher: PasswordHasher = {
     hash: (plain: string) => bcrypt.hash(plain, 10),
     compare: (plain: string, hashed: string) => bcrypt.compare(plain, hashed),
 };
