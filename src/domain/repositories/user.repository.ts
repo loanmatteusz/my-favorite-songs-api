5358dcaa6c6de2f4939a3d778a3cf38bed857892
loanmatteusz <loan.matteusz@gmail.com>
2025-08-28 21:18:28 -0300

    refactor: create function for user repository return User type

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,7 +1,7 @@
 import type { User } from "../entities/user.entity";
 
 export interface UserRepository {
-    create(user: User): Promise<any>;
+    create(user: User): Promise<User>;
     findById(id: string): Promise<any>;
     findByEmail(email: string): Promise<any>;
 }
