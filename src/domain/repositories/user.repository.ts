662c5c456be3f13842f7941b0acbdc3e0d3b9ebb
loanmatteusz <loan.matteusz@gmail.com>
2025-08-29 17:40:10 -0300

    refactor: return user or null in find methods of the user repository interface

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -2,6 +2,6 @@ import type { User } from "../entities/user.entity";
 
 export interface UserRepository {
     create(user: User): Promise<User>;
-    findById(id: string): Promise<any>;
-    findByEmail(email: string): Promise<any>;
+    findById(id: string): Promise<User | null>;
+    findByEmail(email: string): Promise<User | null>;
 }
